6ddc2e82c866f02e020e4c2169110b3974a43c88
unknown <paunkovicwork@gmail.com>
2024-07-30 19:56:18 +0200

    Uredjen kod review. Ispravljene greske

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,46 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\Recipe;
 
 class RecipeController extends Controller
 {
     
-    public function index()
+    public function index():JsonResponse
     {
         $recipes = Recipe::all();
-        return response()->json($recipes,200);
+        return response()->json($recipes);
     }
 
    
-    public function store(Request $request)
+    public function store(Request $request):JsonResponse
     {
         $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
+            'name' => 'required|string|max:32',
             'description' => 'required|string',
-            'kcal' => 'required|numeric',
-            'carbohydrate' => 'required|numeric',
-            'fat' => 'required|numeric',
-            'protein' => 'required|numeric',
+            'kcal' => 'required|numeric|min:1',
+            'carbohydrate' => 'required|numeric|min:1',
+            'fat' => 'required|numeric|min:1',
+            'protein' => 'required|numeric|min:1',
         ]);
-
         $recipe = Recipe::create($validatedData);
 
         return response()->json($recipe, 201);
     }
 
     
-    public function show(string $id)
+    public function show(int $id):JsonResponse
     {
-        $recipe = Recipe::findOrFail($id);
+        // $recipe = Recipe::findOrFail($id);// findOrfail return html
+        $recipe = Recipe::find($id);
+        if ($recipe == null) {
+            return response()->json("Recipe not found",404);// [message=>'Recipe not found']
+        }
         return response()->json($recipe,200);
     }
 
     
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id):JsonResponse
     {
-        $recipe = Recipe::findOrFail($id);
+        $recipe=Recipe::find($id);
+        if ($recipe == null) {
+            return response()->json("Recipe not found",404);// [message=>'Recipe not found']
+        }
 
         $validatedData = $request->validate([
             'name' => 'sometimes|required|string|max:255',
@@ -60,7 +67,7 @@ class RecipeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy( $id)
+    public function destroy($id)// kroz postaman poslati neeki string
     {
         $recipe = Recipe::findOrFail($id);
         $recipe->delete();
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DailyIntakeController;
+use App\Http\Controllers\RecipeController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
 
-
-
+Route::get('/recipes', [RecipeController::class, 'index']);
+Route::put('/recipes/{id}', [RecipeController::class, 'update']);
+Route::post('/recipes', [RecipeController::class, 'store']);
 // Define a GET route to get all users with their ingredient preferences
 Route::get('/users', [UserController::class, 'getAllUsers']);
 
